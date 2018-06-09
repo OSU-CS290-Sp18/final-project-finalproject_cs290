91caee428cbfc5df163770f96687ba36c652ec49
BrandonJ711 <brandoncjacobson7@gmail.com>
2018-06-09 16:17:24 -0700

    bulk of index.js

---
+++ public/index.js
@@ -0,0 +1,51 @@
+
+
+
+
+
+
+
+
+
+/*
+ * Wait until the DOM content is loaded, and then hook up UI interactions, etc.
+ */
+window.addEventListener('DOMContentLoaded', function () {
+
+    // Remember all of the existing twits in an array that we can use for search.
+    var profileElems = document.getElementsByClassName('profile');
+    for (var i = 0; i < profileElems.length; i++) {
+      allProfiles.push(parseProfile(profileELems[i]));
+    }
+  
+    var createProfileButton = document.getElementById('create-profile-button');
+    if (createProfileButton) {
+      createProfileButton.addEventListener('click', showCreateProfileModal);
+    }
+  
+    var modalClose = document.querySelector('#create-profile-modal .modal-close-button');
+    if (modalClose) {
+      modalClose.addEventListener('click', hideCreateProfileModal);
+    }
+  
+    var modalCancelButton = document.querySelector('#create-profile-modal .modal-cancel-button');
+    if (modalCancel) {
+      modalCancel.addEventListener('click', hideCreateProfileModal);
+    }
+  
+    var modalAccept = document.querySelector('#create-profile-modal .modal-accept-button');
+    if (modalAccept) {
+      modalAccept.addEventListener('click', handleModalAccept);
+    }
+  
+    var search = document.getElementById('navbar-search-button');
+    if (search) {
+      searchButton.addEventListener('click', doSearchUpdate);
+    }
+  
+    var searchInput = document.getElementById('navbar-search-input');
+    if (searchInput) {
+      searchInput.addEventListener('input', doSearchUpdate);
+    }
+  
+  });
